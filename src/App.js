ba9a3c8933872967fe655848b9ec5a75c3179486
Remmight <91031667+Remmight@users.noreply.github.com>
2021-12-15 15:10:41 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+//import logo from './logo.svg';
+//import './App.css';
+import AddTask from "./components/AddTask";
+import Task from "./components/Task";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Task />
+        <AddTask />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/AddTask.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import {
+  addTodos,
+  completeTodos,
+  removeTodos,
+  updateTodos,
+} from "../components/reducer";
+import ListTask from "./ListTask";
+
+const mapStateToProps = (state) => {
+  return {
+    todos: state,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addTodo: (obj) => dispatch(addTodos(obj)),
+    removeTodo: (id) => dispatch(removeTodos(id)),
+    updateTodo: (obj) => dispatch(updateTodos(obj)),
+    completeTodo: (id) => dispatch(completeTodos(id)),
+  };
+};
+
+const AddTask = (props) => {
+  const [sort, setSort] = useState("active");
+  return (
+    <div className="addtask">
+      <div className="buttons">
+        <button onClick={() => setSort("active")}>Active</button>
+        <button onClick={() => setSort("completed")}>Completed</button>
+        <button onClick={() => setSort("all")}>All</button>
+      </div>
+      <ul>
+        <>
+          {props.todos.length > 0 && sort === "active"
+            ? props.todos.map((item) => {
+                return (
+                  item.completed === false && (
+                    <ListTask
+                      key={item.id}
+                      item={item}
+                      removeTodo={props.removeTodo}
+                      updateTodo={props.updateTodo}
+                      completeTodo={props.completeTodo}
+                    />
+                  )
+                );
+              })
+            : null}
+          {/* for completed items */}
+          {props.todos.length > 0 && sort === "completed"
+            ? props.todos.map((item) => {
+                return (
+                  item.completed === true && (
+                    <ListTask
+                      key={item.id}
+                      item={item}
+                      removeTodo={props.removeTodo}
+                      updateTodo={props.updateTodo}
+                      completeTodo={props.completeTodo}
+                    />
+                  )
+                );
+              })
+            : null}
+          {/* for all items */}
+          {props.todos.length > 0 && sort === "all"
+            ? props.todos.map((item) => {
+                return (
+                  <ListTask
+                    key={item.id}
+                    item={item}
+                    removeTodo={props.removeTodo}
+                    updateTodo={props.updateTodo}
+                    completeTodo={props.completeTodo}
+                  />
+                );
+              })
+            : null}
+        </>
+      </ul>
+    </div>
+  );
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddTask);
